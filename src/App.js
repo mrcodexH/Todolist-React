a70a7f80505da6d8acf2e0f293072fc1fc062b9b
hasbialwikusmana <hasbialwi70@gmail.com>
2022-06-23 17:26:43 +0700

    deleted item todolist

--- src/App.js
+++ src/App.js
@@ -9,15 +9,27 @@ function App() {
     { todo: "Do laundry", complete: false },
     { todo: "Make Dinner", complete: false },
   ]);
+  // create a new todo item
   const createTodoItem = (todo) => {
     const newTodoItems = [...TodoItems, { todo, complete: false }];
+    setTodoItems(newTodoItems);
+  };
+  // delete the todo item
+  const deleteTodoItem = (index) => {
+    const newTodoItems = [...TodoItems];
+    newTodoItems.splice(index, 1);
     setTodoItems(newTodoItems);
   };
   return (
     <div className="App">
       <TodoInput createTodoItem={createTodoItem} />
       {TodoItems.map((item, index) => (
-        <TodoItem key={index} index={index} item={item} />
+        <TodoItem
+          key={index}
+          index={index}
+          item={item}
+          deleteTodoItem={deleteTodoItem}
+        />
       ))}
     </div>
   );
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const TodoItem = ({ item, index }) => {
+const TodoItem = ({ item, index, deleteTodoItem }) => {
   return (
     <div className="todo-list">
       <li> {item.todo} </li>
+      <div className="btns">
+        <button onClick={() => deleteTodoItem(index)}>Delete</button>
+      </div>
     </div>
   );
 };
